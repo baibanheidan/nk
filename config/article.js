be2600604a72d952bb828fbd1147c31af2e89b7e
baibanheidan <2195278757@qq.com>
2018-12-24 13:43:48 +0800

    1.md

--- config/article.js
+++ config/article.js
@@ -2,6 +2,10 @@ rightCount = 10 // 右侧边栏显示条数，查找不受此限制
 markdir = "mds/" // markdown文件存放地址
 marks = [ // 仓库中的文件列表
 	{
+		"name": "1",
+		"tags":["second","笔记"]
+	},
+	{
 		"name": "项目介绍",
 		"tags":["second","demo"]
 	}
